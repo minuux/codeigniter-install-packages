a4af3b587eb91614c205e5e9bc559eb0847b24b7
minuux <61769459@qq.com>
2017-02-18 00:33:57 +0800

    Fix error class name

--- src/Composer/Installer/CodeigniterInstallPackages.php
+++ src/Composer/Installer/CodeigniterInstallPackages.php
@@ -5,7 +5,7 @@ namespace Composer\Installer;
 use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
 
-class CodeigniterInstallerPackages extends LibraryInstaller{
+class CodeigniterInstallPackages extends LibraryInstaller{
 
     public function getInstallPath(PackageInterface $package){
         throw new \InvalidArgumentException("Package install path ${parent::getInstallPath($package)}");
