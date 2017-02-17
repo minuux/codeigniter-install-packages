da7d07073b25d1f3c4cfc4a598b5325a888c273f
minuux <61769459@qq.com>
2017-02-18 00:53:59 +0800

    ...

--- src/Composer/Installer/CodeigniterInstallPackages.php
+++ src/Composer/Installer/CodeigniterInstallPackages.php
@@ -8,9 +8,6 @@ use Composer\Repository\InstalledRepositoryInterface;
 
 class CodeigniterInstallPackages extends LibraryInstaller{
 
-    public function getInstallPath(PackageInterface $package){
-        throw new \InvalidArgumentException("Package install path ${parent::getInstallPath($package)}");
-        return parent::getInstallPath($package);
-    }
+    
 
 }
