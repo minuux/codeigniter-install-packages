f87d725666c6209d1026fae8370a992a64d07a8e
minuux <61769459@qq.com>
2017-02-18 21:53:40 +0800

    DONE

--- src/Composer/Installer/CodeigniterInstallPackages.php
+++ src/Composer/Installer/CodeigniterInstallPackages.php
@@ -4,13 +4,41 @@ namespace Composer\Installer;
 
 use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
-use Composer\Repository\InstalledRepositoryInterface;
 
 class CodeigniterInstallPackages extends LibraryInstaller{
 
-    public function getInstallPath(PackageInterface $package){
-        throw new \InvalidArgumentException("Package install path ${parent::getInstallPath($package)}");
-        return parent::getInstallPath($package);
-    }
+    protected function installCode(PackageInterface $package)
+	{
+		parent::installCode($package);
+		$this->postInstallActions($package);
+	}
+
+	protected function postInstallActions($package){
+
+		if(!in_array($package->getType(),array('codeigniter-view','codeigniter-model','codeigniter-library','codeigniter-help'))){
+			return;
+		}
+		$prefix=rtrim($package->getType(),'codeigniter-');
+		$configPath=dirname(rtrim($this->composer->getConfig()->get('vendor-dir'), '/')).'/application/config/';
+    	$packagePath=$configPath.'packages.php';
+    	// @HACK to work around the security check in CI config files
+    	if ( ! defined('BASEPATH')){
+			define('BASEPATH', 1);
+		}
+    	
+    	if (file_exists($packagePath)){
+			@include($packagePath);
+		}else{
+			$config=array();
+		}
+		$config[$package->getPrettyName()]=parent::getInstallPath($package);
+    	
+    	$str_tmp="<?php\r\n"; //得到php的起始符。$str_tmp将累加
+		$str_tmp.="defined('BASEPATH') OR exit('No direct script access allowed');\r\n";
+		foreach ($config as $key => $value) {
+			$str_tmp.="\$config['$prefix']['$key']='$value';\r\n";
+		}
+		file_put_contents($packagePath,$str_tmp);
+	}
 
 }
