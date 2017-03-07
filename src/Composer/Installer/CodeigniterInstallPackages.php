baa9dbdb7c059e4b88a660fd34feb74c9d79427a
li <61769459@qq.com>
2017-03-07 13:02:14 +0800

    fix view,helper have multiple files
    remove class_name valid
    resolve codeigniter-view,codeigniter-helper have multiple files

--- src/Composer/Installer/CodeigniterInstallPackages.php
+++ src/Composer/Installer/CodeigniterInstallPackages.php
@@ -11,42 +11,50 @@ class CodeigniterInstallPackages extends LibraryInstaller{
 		return in_array($packageType, array('codeigniter-view','codeigniter-model','codeigniter-library','codeigniter-helper'));
 	}
 
-    protected function installCode(PackageInterface $package)
-	{
+    	protected function installCode(PackageInterface $package){
 		parent::installCode($package);
 		$this->postInstallActions($package);
 	}
 
 	protected function postInstallActions($package){
+		
 		$extra = $package->getExtra();
 
-		if(!isset($extra['main'])){
+		if(in_array($package->getType(),array('codeigniter-model','codeigniter-library')) && !isset($extra['main'])){
 			throw new \InvalidArgumentException("extra's main is require");
-		}
-		if(in_array($package->getType(),array('codeigniter-model','codeigniter-library')) && !isset($extra['class_name'])){
-			throw new \InvalidArgumentException("extra's class_name is require");
 		}
 
 		$prefix=rtrim($package->getType(),'codeigniter-');
 		$configPath=dirname(rtrim($this->composer->getConfig()->get('vendor-dir'), '/')).'/application/config/';
-    	$packagePath=$configPath.'packages.php';
-    	// @HACK to work around the security check in CI config files
-    	if ( ! defined('BASEPATH')){
+    		$packagePath=$configPath.'packages.php';
+    		// @HACK to work around the security check in CI config files
+    		if ( ! defined('BASEPATH')){
 			define('BASEPATH', 1);
 		}
     	
-    	if (file_exists($packagePath)){
+    		if (file_exists($packagePath)){
 			@include($packagePath);
 		}else{
 			$config=array();
 		}
-		$config[$package->getPrettyName()]=array(
-			'path'=>$this->getInstallPath($package).'/'.$extra['main'],
-			'type'=>$package->getType(),
-			'class_name'=>$extra['class_name']
-		);
+		
+		if(in_array($package->getType(),array('codeigniter-model','codeigniter-library'))){
+			$config[$package->getPrettyName()]=array(
+				'path'=>$this->getInstallPath($package).'/'.$extra['main'],
+				'type'=>$package->getType(),
+				'class_name'=>rtrim($extra['main'],'.php')
+			);
+		}else{
+			foreach(glob($this->getInstallPath($package).'/*.php') as $item_filename){
+				//获取文件的名称并去除.php后缀
+				$config[$package->getPrettyName().':'.rtrim(basename($item_filename),'.php')]=array(
+					'path'=>$item_filename,
+					'type'=>$package->getType()
+				);
+			}
+		}
     	
-    	$str_tmp="<?php\r\n"; //得到php的起始符。$str_tmp将累加
+    		$str_tmp="<?php\r\n"; //得到php的起始符。$str_tmp将累加
 		$str_tmp.="defined('BASEPATH') OR exit('No direct script access allowed');\r\n";
 		//http://stackoverflow.com/questions/18342477/save-array-to-php-file
 		file_put_contents($packagePath,$str_tmp.'$config='. var_export($config, true) . ';');
